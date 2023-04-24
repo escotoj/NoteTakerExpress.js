80c1702d21147ee217e925190d31f3300ad236eb
escotoj <j.escoto14@gmail.com>
2023-04-24 16:41:12 -0700

    got the routers folder to work, and the db.json get and post to work

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,46 @@
 const router = require('express').Router();
+// const path = require('path');
+const fs = require('fs');
 
 
+router.get('/api/notes', (req, res) => {
+    fs.readFile('Develop/db/db.json', 'utf8', (err, notes) => {
+        if (err) {
+          return res.status(500).json({ err });
+        }
+        res.json(JSON.parse(notes));
+    });
+  });
+  // path.join(__dirname, '..', 'db', 'db.json')
+
+
+  router.post('/api/notes', (req, res) => {
+  const { title, text, id } = req.body;
+  if (title && text && id ) {
+    fs.readFile('Develop/db/db.json', 'utf8', (err, notes) => {
+      if (err) {
+        return res.status(500).json({ err });
+      }
+      const data = JSON.parse(notes);
+      data.push ({
+        id,
+        title,
+        text
+      })
+  fs.writeFile('Develop/db/db.json', JSON.stringify(data, null, 2), (err) =>{
+    if (err) {
+      return res.status(500).json({ err });
+    }
+  res.json({ id, title, text });
+  });
+  
+  });
+  
+  } else {
+    res.status(500).json({ error: "title and text are required" });
+  }
+  
+  });
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 // add routes file 
-// const routes = require('./routes');
+const routes = require('./routes');
 const app = express();
 
 
@@ -13,44 +13,44 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// app.use(routes);
+app.use(routes);//
 
-app.get('/api/notes', (req, res) => {
-    fs.readFile('db/db.json', 'utf8', (err, notes) => {
-        if (err) {
-          return res.status(500).json({ err });
-        }
-        res.json(JSON.parse(notes));
-    });
-  });
+// app.get('/api/notes', (req, res) => {
+//     fs.readFile('db/db.json', 'utf8', (err, notes) => {
+//         if (err) {
+//           return res.status(500).json({ err });
+//         }
+//         res.json(JSON.parse(notes));
+//     });
+//   });
 
 
-  app.post('/api/notes', (req, res) => {
-  const { title, text, id } = req.body;
-  if (title && text && id ) {
-    fs.readFile('db/db.json', 'utf8', (err, notes) => {
-      if (err) {
-        return res.status(500).json({ err });
-      }
-      const data = JSON.parse(notes);
-      data.push ({
-        id,
-        title,
-        text
-      })
-  fs.writeFile('db/db.json', JSON.stringify(data, null, 2), (err) =>{
-    if (err) {
-      return res.status(500).json({ err });
-    }
-  res.json({ id, title, text });
-  });
+//   app.post('/api/notes', (req, res) => {
+//   const { title, text, id } = req.body;
+//   if (title && text && id ) {
+//     fs.readFile('db/db.json', 'utf8', (err, notes) => {
+//       if (err) {
+//         return res.status(500).json({ err });
+//       }
+//       const data = JSON.parse(notes);
+//       data.push ({
+//         id,
+//         title,
+//         text
+//       })
+//   fs.writeFile('db/db.json', JSON.stringify(data, null, 2), (err) =>{
+//     if (err) {
+//       return res.status(500).json({ err });
+//     }
+//   res.json({ id, title, text });
+//   });
   
-  });
+//   });
   
-  } else {
-    res.status(500).json({ error: "title and text are required" });
-  }
+//   } else {
+//     res.status(500).json({ error: "title and text are required" });
+//   }
   
-  });
+//   });
   
   app.listen(PORT, () => console.log(`Server listening on PORT http://localhost:${PORT}`));
